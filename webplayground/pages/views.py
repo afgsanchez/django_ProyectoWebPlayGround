4b9db8c9e6c1a5a14ca091ffb7c5977ad26d3a6e
Antonio Gutierrez Sanchez <afgsanchez@gmail.com>
2025-05-04 22:41:11 +0200

    updates

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,6 +1,9 @@
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .forms import PageForm
 
 # Create your views here.
 class PageListView(ListView):
@@ -8,3 +11,20 @@ class PageListView(ListView):
 
 class PageDetailView(DetailView):
      model = Page
+
+class PageCreate(CreateView):
+    model = Page
+    form_class = PageForm
+    success_url = reverse_lazy('pages:pages')
+
+class PageUpdate(UpdateView):
+    model = Page
+    form_class = PageForm
+    template_name_suffix = "_update_form"
+
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + "?ok"
+
+class PageDelete(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
