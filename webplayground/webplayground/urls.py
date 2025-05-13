5ec4f7ce1dcb1c336c6b40bbb2eb3eb43205d7e5
Antonio Gutierrez Sanchez <afgsanchez@gmail.com>
2025-05-13 19:57:57 +0200

    updates

--- webplayground/webplayground/urls.py
+++ webplayground/webplayground/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Paths de Auth
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.urls')),
+
 ]
