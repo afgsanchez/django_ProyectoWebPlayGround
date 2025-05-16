5ab68503b52a7d9ee0b87a9d9cf263ae5453dc0c
Antonio Gutierrez Sanchez <afgsanchez@gmail.com>
2025-05-16 21:09:12 +0200

    updates

--- webplayground/registration/models.py
+++ webplayground/registration/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 
 # Create your models here.
 class Profile(models.Model):
@@ -10,3 +13,11 @@ class Profile(models.Model):
 
     # def __str__(self):
     #     return self.username
+
+@receiver(post_save, sender=User)
+def ensure_profile_exists(sender, instance, **kwargs):
+    if kwargs.get('created', False):
+        # Create a Profile instance for the new User
+        # This will create a Profile instance if it doesn't exist
+        Profile.objects.get_or_create(user=instance)
+        print("Profile created for user:", instance.username)
