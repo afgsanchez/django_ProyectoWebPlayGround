4aaaa4575801d9ef4a839da03c642011c9e93b80
Antonio Gutierrez Sanchez <afgsanchez@gmail.com>
2025-05-16 21:34:37 +0200

    optimized avatar

--- webplayground/registration/tests.py
+++ webplayground/registration/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        # Create a user instance
+        User.objects.create_user('test', 'test@test.com', 'test1234')
+
+    def test_profile_exists(self):
+        # Check if the profile was created successfully
+        exists = Profile.objects.filter(user__username='test').exists()
+        self.assertEqual(exists, True)
