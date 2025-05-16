9a389ead97496d2d87e0c677e32708c7dacf0475
Antonio Gutierrez Sanchez <afgsanchez@gmail.com>
2025-05-16 20:46:11 +0200

    added option change email

--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
-from .forms import UserCreationFormWithEmail, ProfileForm
+
+from .forms import UserCreationFormWithEmail, ProfileForm, EmailForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django import forms
@@ -34,3 +34,19 @@ class ProfileUpdate(UpdateView):
     def get_object(self):
         profile, created = Profile.objects.get_or_create(user=self.request.user)
         return profile
+
+@method_decorator(login_required, name='dispatch')
+class EmailUpdate(UpdateView):
+    form_class = EmailForm
+    success_url = reverse_lazy('profile')
+    template_name = 'registration/profile_email_form.html'
+
+    def get_object(self):
+        return self.request.user
+    
+    def get_form(self, form_class=None):
+        form = super(EmailUpdate, self).get_form()
+        #Modificamos en tiempo real el formulario
+        form.fields['email'].widget = forms.EmailInput(
+            attrs={'class': 'form-control mb-2', 'placeholder': 'Dirección email'})
+        return form
